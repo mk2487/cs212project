4935e770208de0da8a51e30993799055c52c6533
Angel Sotelo-Ayala <angel.pug@icloud.com>
2025-11-17 14:40:07 -0700

    Add files via upload
    I redid all of the coloring on both light and dark mode to look better than the first implementation. I also added a hover effect to the cards.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Important RGB Values to be consistent with:
+// if you want to add more or change just make sure to update here and in the code accordingly
+
+// white rgb value is (255, 255, 255)
+// black rgb value is (0, 0, 0)
+// darker grey rgb value is (20, 20, 20)
+// dark grey rgb value is (40, 40, 40)
+// light grey rgb value is (240, 240, 240)
+
 document.addEventListener("DOMContentLoaded", function () 
 {
     //Smooth Scrolling
@@ -30,27 +39,99 @@ function bgColorChange()
     // get the current background color
     const currentBG = getComputedStyle(body).backgroundColor;
 
-    //  check if current color is white
+    //  check if current color is white -> change to black theme
     if (currentBG === 'rgb(255, 255, 255)')
     {
-        // switch to dark mode
-        body.style.backgroundColor = 'rgb(18, 18, 18)';
+        // Header only has text to change color
+            // save header element to a variable
+        const header = document.querySelector("header");
+            // change header text color to white in dark mode
+        header.style.color = 'rgb(255, 255, 255)';
+
+        // now get navbar and change its background and link colors
+        const navbar = document.getElementById("navbar");
+        const navabarLinks = document.querySelectorAll(".nav-item a");
+
+            // change navbar background to dark grey in dark mode
+        navbar.style.backgroundColor = 'rgb(40, 40, 40)';
+            // change navbar link colors to white in dark mode
+        navabarLinks.forEach(link => {
+            link.style.color = 'rgb(255, 255, 255)';
+        });
+
+
+        // changing the body background to black
+        body.style.backgroundColor = 'rgb(20, 20, 20)';
+        // changing the body text to white
         body.style.color = 'rgb(255, 255, 255)';
 
-        // change icon to sun
+        // change icon to sun for dark mode
         const icon = document.querySelector("i");
         icon.classList = "fa fa-sun";
+
+        // change the cards to be greyish in dark mode for all under id teams
+        const cards = document.querySelectorAll("#team .card-body");
+        cards.forEach(card => {
+            card.style.backgroundColor = 'rgb(40, 40, 40)';
+            card.style.color = 'rgb(255, 255, 255)';
+        });
+
+        // change the contact section to be greyish in dark mode
+        const contactSection = document.getElementById("contact");
+        contactSection.style.backgroundColor = 'rgb(40, 40, 40)';
+        // change contact section text to white in dark mode
+        contactSection.style.color = 'rgb(255, 255, 255)';
+
+        // change the footer background to dark grey in dark mode
+        const footer = document.querySelector("footer");
+        footer.style.backgroundColor = 'rgb(20, 20, 20)';
     }
 
-    // if current color is black
-    else if (currentBG === 'rgb(18, 18, 18)')
+    // if current color is black then change to white theme
+    else if (currentBG === 'rgb(20, 20, 20)')
     {
-        // switch to light mode
+        // Header only has text to change color
+            // save header element to a variable
+        const header = document.querySelector("header");
+            // change header text color to black in light mode
+        header.style.color = 'rgb(0, 0, 0)';
+
+        // now get navbar and change its background and link colors
+        const navbar = document.getElementById("navbar");
+        const navabarLinks = document.querySelectorAll(".nav-item a");
+
+            // change navbar background to light grey in light mode
+        navbar.style.backgroundColor = 'rgb(240, 240, 240)';
+            // change navbar link colors to black in light mode
+        navabarLinks.forEach(link => {
+            link.style.color = 'rgb(0, 0, 0)';
+        });
+
+        // chainging the body background to white
         body.style.backgroundColor = 'rgb(255, 255, 255)';
+        // chainging the body text to black
         body.style.color = 'rgb(0, 0, 0)';
 
         // change icon to moon
         const icon = document.querySelector("i");
         icon.classList = "fa fa-moon";
+
+        // change the cards to be white in light mode for all under id teams
+        const cards = document.querySelectorAll("#team .card-body");
+        // change the card text color to black in light mode
+        cards.forEach(card => {
+            card.style.backgroundColor = 'rgb(255, 255, 255)';
+            card.style.color = 'rgb(0, 0, 0)';
+        });
+
+        // change the contact section to be light grey in light mode
+        const contactSection = document.getElementById("contact");
+        contactSection.style.backgroundColor = 'rgb(240, 240, 240)';
+        // setting contact section text to black in light mode
+        contactSection.style.color = 'rgb(0, 0, 0)';
+
+        // change the footer background to white in light mode
+        const footer = document.querySelector("footer");
+        footer.style.backgroundColor = 'rgb(255, 255, 255)';
     }
 }
