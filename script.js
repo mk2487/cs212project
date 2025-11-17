e5530743420d86c4f39f77e79b1b6c92201fb6e2
Angel Sotelo-Ayala <angel.pug@icloud.com>
2025-11-17 08:21:31 -0700

    Add files via upload
    Fixed the dark mode button and animation to the moon/sun icon. Also added the CSS and JS as they were not linked properly.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () 
+{
     //Smooth Scrolling
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener("click", function (e) {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => 
+    {
+        anchor.addEventListener("click", function (e) 
+        {
          
             if (this.getAttribute("href") === "#") return;
 
             e.preventDefault();
             const target = document.querySelector(this.getAttribute("href"));
-            if (target) {
+            if (target) 
+            {
                 target.scrollIntoView({
                     behavior: "smooth",
                     block: "start"
@@ -15,18 +19,38 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-
-
-    const toggleBtn = document.getElementById("theme-toggle");
-    const icon = toggleBtn?.querySelector("i");
-
-    if (toggleBtn) {
-        toggleBtn.addEventListener("click", () => {
-            document.body.classList.toggle("dark-mode");
-
-
-            const dark = document.body.classList.contains("dark-mode");
-            icon.classList = dark ? "fa fa-sun" : "fa fa-moon";
-        });
-    }
 });
+
+// this function toggles the background color between light and dark mode
+function bgColorChange()
+{
+    // var to hold the color element theme-toggle
+    const body = document.getElementById("theme-toggle");
+
+    // get the current background color
+    const currentBG = getComputedStyle(body).backgroundColor;
+
+    //  check if current color is white
+    if (currentBG === 'rgb(255, 255, 255)')
+    {
+        // switch to dark mode
+        body.style.backgroundColor = 'rgb(18, 18, 18)';
+        body.style.color = 'rgb(255, 255, 255)';
+
+        // change icon to sun
+        const icon = document.querySelector("i");
+        icon.classList = "fa fa-sun";
+    }
+
+    // if current color is black
+    else if (currentBG === 'rgb(18, 18, 18)')
+    {
+        // switch to light mode
+        body.style.backgroundColor = 'rgb(255, 255, 255)';
+        body.style.color = 'rgb(0, 0, 0)';
+
+        // change icon to moon
+        const icon = document.querySelector("i");
+        icon.classList = "fa fa-moon";
+    }
+}
